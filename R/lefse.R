c6a259a3f6ee85344cdb8f8b1fd01e145c70b28e
tmbuza <ndelly@gmail.com>
2022-06-12 08:56:24 -0500

    Create lefse.R

---
+++ R/lefse.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(ggtext)
+library(glue)
+library(broom)
+
+set.seed(110912)
+
+metadata <- readRDS("~/Dropbox/CDILLC/GIT_REPOS/smda-end2end/RDataRDS/mo_metadata.rds") %>% 
+  select(sample_id, isolate) %>% 
+  drop_na(isolate)
+
+shared_file <- read_tsv("~/Dropbox/CDILLC/GIT_REPOS/smda-end2end/data/final.tx.1.subsample.shared", show_col_types = F)
+
+
+shared_design <- inner_join(shared_file, metadata, by=c("Group" = "sample_id"))
+# shared_design %>% 
+#   colnames() %>% 
+#   tail
+
+run_lefse <- function(x, y, tag) {
+  x_y <- shared_design %>%
+    filter(isolate == x | isolate == y)
+  
+  x_y %>% 
+    select(-isolate) %>% 
+    write_tsv(glue("RDataRDS//bush_{tag}.shared"))
+  
+  x_y %>% 
+    select(Group, isolate) %>% 
+    write_tsv(glue("RDataRDS//bush_{tag}.design"))
+  
+  command <- glue('mothur/mothur "#lefse(shared=bush_{tag}.shared, design=bush_{tag}.design, inputdir=RDataRDS, outputdir=RDataRDS/mothur_out)"')
+  
+  system(command)
+  
+  return(glue("bush_{tag}.1.lefse_summary"))
+  
+}
